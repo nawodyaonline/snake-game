caa6a4729d14267e128389a542f286613de38597
nawodyaonline <nawodyahckr@gmail.com>
2020-12-23 23:48:50 +0530

    Added up down left right function to snake

--- snake.py
+++ snake.py
@@ -25,17 +25,13 @@ class Snake():
 
     
     def up(self):
-        for seg_num in range(len(self.segments) - 1, 0, -1):
-            new_x = self.segments[seg_num - 1].xcor()
-            new_y = self.segments[seg_num - 1].ycor()
-            self.segments[seg_num].goto(new_x, new_y)   
-        self.segments[0].setheading[90]
+        self.segments[0].setheading(90)
 
     def down(self):
-        self.segments[0].setheading[270]
+        self.segments[0].setheading(270)
 
     def left(self):
-        self.segments[0].setheading[180]
+        self.segments[0].setheading(180)
 
     def right(self):
-        self.segments[0].setheading[0]
+        self.segments[0].setheading(0)
